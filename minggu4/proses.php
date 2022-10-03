812d7d23e0cb80e01ee8ed0037c35a61b6baae66
Fernando Tambun <111202214723@mhs.dinus.ac.id>
2022-10-03 20:02:54 +0700

    Minggu4

---
+++ minggu4/proses.php
@@ -0,0 +1,15 @@
+<?php
+    print_r($_POST);
+
+    $nim = $_POST['nim']
+    $nama = $_POST['nama']
+    $prodi = $_POST['prodi']
+    $bidang = $_POST['bidang']
+?>
+<p>
+Nama <b><?php echo $nama;?><br></b>
+NIM <b><?php echo $nama;?><br></b>
+Prodi <b><?php echo $nama;?><br></b>
+Bidang <b><?php echo $nama;?> <br></b>
+
+</p>
